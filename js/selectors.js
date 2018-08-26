b2d1b3754232078a5eef8a63f4705711d9c6d486
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-26 16:55:28 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,17 +3,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
-  $('ul #pic-list:first-child');
+  return $('ul #pic-list:first-child');
 }
